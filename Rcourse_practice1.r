5d2175751294eb3d06e60deb749a69c0a9b353b4
turingskj <turingskj@hotmail.com>
2020-03-27 01:08:22 -0400

    table and droplevels practice

--- Rcourse_practice1.r
+++ Rcourse_practice1.r
@@ -1,4 +1,10 @@
-lets = sample(letters, size=100, replace=TRUE)
-lets = factor(lets)
+lets <- sample(LETTERS, size=10, replace=TRUE)
+lets <- factor(lets, levels=LETTERS)
+table(lets)
+table(droplevels(lets))
+
+
+temp1<-as.data.frame(table(lets1))
 
 print("This is testing RStudio with Github...")
+
